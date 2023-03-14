482daf9f7471164356d580af4649504b986702cd
avishka2k <avishka2k@gmail.com>
2023-03-14 23:33:55 +0530

    Add braille windows app project files

---
+++ BrailleApp/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace BrailleApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.Padding = new System.Drawing.Point(30, 20);
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(873, 600);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage1.Controls.Add(this.errorLabel);
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.radioButton2);
+            this.tabPage1.Controls.Add(this.radioButton1);
+            this.tabPage1.Controls.Add(this.numericUpDown2);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.numericUpDown1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 61);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(865, 535);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Shapes";
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.errorLabel.Location = new System.Drawing.Point(337, 100);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(41, 15);
+            this.errorLabel.TabIndex = 11;
+            this.errorLabel.Text = "label3";
+            this.errorLabel.Visible = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(519, 118);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(274, 273);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(273, 47);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(139, 176);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(77, 24);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Normal";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(58, 176);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(70, 24);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Braille";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(211, 256);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown2.TabIndex = 6;
+            this.numericUpDown2.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(207, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(58, 256);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownHeight = 100;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.Location = new System.Drawing.Point(58, 94);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(273, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Select Shape";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.richTextBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 61);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(865, 535);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Text";
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(429, 183);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 49);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Convert";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(31, 65);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(351, 242);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(873, 600);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label errorLabel;
+    }
+}
+
---
+++ BrailleApp/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace BrailleApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //ComboBox comboBox = new ComboBox();
+            ShapesItem();       
+        }
+
+        private void ShapesItem()
+        {
+            string[] shapes = {"Square", "Rectangle", "Pyramid", "Right Triangle", "Left Triangle",
+                   "Diamond", "Circle", "Left arrow", "Right arrow", "Hexagon", "Heart",
+                   "Crown", "Christmas tree", "Wave", "X mark", "Star"};
+            Array.Sort(shapes);
+            comboBox1.Items.AddRange(shapes);
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ShapesItem();
+            string selectedItem = comboBox1.SelectedItem.ToString();
+
+            switch (selectedItem)
+            {
+                case "Square":
+                    string textValue = numericUpDown1.Text;
+                    Rectangle_Shape(textValue, textValue, false);
+                    break;
+                case "Rectangle":
+                    string textValue1 = numericUpDown1.Text;
+                    string textValue2 = numericUpDown2.Text;
+                    Rectangle_Shape(textValue1, textValue2, true);
+                    break;
+                case "Pyramid":
+                    Pyramid_Shape();
+                    break;
+                case "Circle":
+                    Circle_Shape();
+                    break;
+                case "Right Triangle":
+                    RightTriangle_Shape();
+                    break;
+                case "Left Triangle":
+                    LeftTriangle_Shape();
+                    break;
+                case "Diamond":
+                    Diamond_Shape();
+                    break;
+                case "Heart":
+                    Heart_Shape();
+                    break;
+                default:           
+                    break;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            errorLabel.ForeColor = Color.Red;
+            if (comboBox1.SelectedItem == null)
+            {
+                errorLabel.Show();
+                errorLabel.Text = "Select Item";
+                return;
+            }
+            errorLabel.Hide();
+            errorLabel.Text = "";
+            comboBox1_SelectedIndexChanged(sender,e);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Rectangle, Square
+        private void Rectangle_Shape(string textValue1, string textValue2, bool second)
+        {
+            label1.Show();
+            numericUpDown1.Show();
+            label1.Text = "Side length";
+            label2.Hide();
+            numericUpDown2.Hide();
+
+            Second_Value(second);
+            string url = $"http://localhost:8089/DotPattern/api/rectangle/{textValue1}/{textValue2}";
+            GetApi(url);
+        }
+
+        private void Second_Value(bool second)
+        {
+            if (second)
+            {
+                label2.Show();
+                numericUpDown2.Show();
+                label1.Text = "Width";
+                label2.Text = "Height";
+            }
+        }
+
+        private void Parms(string labelText1, string labelText2, bool second)
+        {
+            label1.Show();
+            numericUpDown1.Show();
+            label1.Text = labelText1;
+            label2.Hide();
+            numericUpDown2.Hide();
+
+            if (second)
+            {
+                label2.Show();
+                numericUpDown2.Show();
+                label2.Text = labelText2;
+            }
+        }
+        //Circle
+        private void Circle_Shape()
+        {
+            Parms("Radius", "", false);
+            string textValue1 = numericUpDown1.Text;
+            string textValue2 = "3";
+
+            string url = $"http://localhost:8089/DotPattern/api/circle/{textValue1}/{textValue2}";
+            GetApi(url);
+        }
+
+        //Pyramid
+        private void Pyramid_Shape()
+        {
+            Parms("Width", "Height", true);
+            string textValue1 = numericUpDown1.Text;
+            string textValue2 = numericUpDown2.Text;
+            string url = $"";
+            GetApi(url);
+        }
+
+        //RightTriangle
+        private void RightTriangle_Shape()
+        {
+            Parms("Size", "", false);
+            string textValue1 = numericUpDown1.Text;
+            string url = $"";
+            GetApi(url);
+
+        }
+
+        //LeftTriangle
+        private void LeftTriangle_Shape()
+        {
+            Parms("Size", "", false);
+            string textValue1 = numericUpDown1.Text;
+            string url = $"";
+            GetApi(url);
+        }
+
+        //Diamond
+        private void Diamond_Shape()
+        {
+            Parms("Width", "Height", true);
+        }
+
+        //Heart
+        private void Heart_Shape()
+        {
+            Parms("Size", "", false);
+
+        }
+
+        //Get Api data
+        private async void GetApi(string url)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync(url);
+                string dotPattern = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    richTextBox1.Text = dotPattern;
+                }
+                else
+                {
+                    richTextBox1.Text = "Server Error";
+                }
+            }catch(Exception ex)
+            {
+                richTextBox1.Text = ex.Message;
+            }
+            
+        }
+
+        
+    }
+}
